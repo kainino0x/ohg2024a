3b39c92b66ac1791d34b9e469945d641afccce55
Kai Ninomiya <kainino1@gmail.com>
2024-01-20 20:20:29 +0000

    RIDGE

--- docs/main.js
+++ docs/main.js
@@ -4,7 +4,7 @@ import { keyboard } from "./keyboard.js";
 // Setup
 
 const app = new PIXI.Application({
-  background: "#1099bb",
+  background: "#44ccff",
   //resizeTo: divMain,
 });
 divMain.appendChild(app.view);
@@ -22,28 +22,76 @@ const music = PIXI.sound.Sound.from({
 
 // Sprites
 
-const bunny = PIXI.Sprite.from("assets/bunny.png");
+const ridges = [];
+let advanceRidgeTo;
 {
-  bunny.anchor.set(0.5, 0.5);
-  bunny.x = app.screen.width / 2;
-  bunny.y = app.screen.height / 2;
+  for (let i = 0; i < 40; ++i) {
+    const r = (ridges[i] = PIXI.Sprite.from("assets/ridge.png"));
+    r.scale.set(20, 20);
+    r.x = i * 20;
 
-  bunny.eventMode = "static";
-  bunny.cursor = "pointer";
-  bunny.on("pointerdown", () => {
-    bunny.scale.x *= 1.25;
-    bunny.scale.y *= 1.25;
-  });
+    app.stage.addChild(r);
+  }
 
-  app.stage.addChild(bunny);
+  let ridgeHeight = 200;
+  let lastTime = 0;
+  let ringBufferIndex = 0;
+  const advanceRidge = () => {
+    ridgeHeight = Math.max(
+      200,
+      Math.min(500, ridgeHeight + (Math.random() * 2 - 1) * 20)
+    );
+    for (let i = 0; i < 39; ++i) {
+      ridges[i].y = ridges[i + 1].y;
+    }
+    ridges[39].y = ridgeHeight;
+    ringBufferIndex = (ringBufferIndex + 1) % 40;
+  };
+  for (let i = 0; i < 40; ++i) {
+    advanceRidge();
+  }
+
+  advanceRidgeTo = (time) => {
+    if (lastTime < time) {
+      advanceRidge();
+      lastTime += 50;
+      return true;
+    }
+    return false;
+  };
 }
 
-const wave = new PIXI.Text("o/");
+const skier = PIXI.Sprite.from("assets/skier.png");
 {
-  wave.x = 50;
-  wave.y = 100;
+  skier.anchor.set(0.6, 0.8);
+  skier.x = 295;
 
-  app.stage.addChild(wave);
+  //skier.eventMode = "static";
+  //skier.cursor = "pointer";
+  //skier.on("pointerdown", () => {
+  //  skier.scale.x *= 1.25;
+  //  skier.scale.y *= 1.25;
+  //});
+
+  app.stage.addChild(skier);
+}
+
+const text = new PIXI.Text("100");
+{
+  text.x = 600;
+  text.y = 50;
+  text.style.fontSize = 50;
+  text.pivot.set(text.width * 0.45, text.height * 0.65);
+
+  app.stage.addChild(text);
+}
+
+const circle = new PIXI.Graphics();
+{
+  circle.beginFill(0xffffff);
+  circle.drawCircle(30, 30, 30);
+  circle.endFill();
+  app.stage.addChild(circle);
 }
 
 // Keypresses
@@ -56,14 +104,32 @@ window.addEventListener("keydown", (ev) => {
 
 // Frame
 
+let time = 0;
+let nextRidgeDelta = 0;
+let hp = 100;
 app.ticker.add((delta) => {
-  bunny.rotation -= 0.01 * delta;
-  wave.x += keyboard.dir.x;
-  wave.y += keyboard.dir.y;
+  time += delta;
 
-  if (collides(wave, bunny)) {
-    bunny.tint = 0x00ff00;
-  } else {
-    bunny.tint = 0xffffff;
+  skier.y = ridges[14].y;
+  skier.rotation = keyboard.dir.x * 0.4;
+  const midHeightTarget = -keyboard.dir.x * 10;
+  const minHeightTarget = midHeightTarget - 10.01;
+  const maxHeightTarget = midHeightTarget + 10.01;
+
+  const pass = minHeightTarget < nextRidgeDelta && nextRidgeDelta < maxHeightTarget;
+  ridges[14].tint = pass ? 0x8888ff : 0xffff00;
+
+  if (advanceRidgeTo(time)) {
+    ridges[14].tint = 0x8888ff;
+    nextRidgeDelta = ridges[14].y - ridges[15].y;
+    console.log(nextRidgeDelta, minHeightTarget, maxHeightTarget);
+
+    if (!pass) {
+      hp--;
+      text.text = hp;
+      text.style.fill = 'red';
+    } else {
+      text.style.fill = 'black';
+    }
   }
 });
